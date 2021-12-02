dfa4061164c20f6439f42fa3966c0cadf416beba
Ubuntu <ubuntu@ip-172-31-84-55.ec2.internal>
2021-12-02 01:58:39 +0000

    final commit

--- Array.swift
+++ Array.swift
@@ -1,79 +1,128 @@
 /*
-* This array gives the mean medium and mode of a file.
+* This is a program that calculates mean, median and mode
+* after reading in a text file into an array.
 *
-* @author  Jonathan Pasco-Arnone
+* @author  Felipe Garcia Affonso
 * @version 1.0
-* @since   2021-11-30
+* @since   2021-11-26
 */
-
-// Swift "Statistics" Program
 
 import Foundation
 
-func mean(arrayOfIntegers: [Int]) throws -> Double {
-  // this is the mean function.
-  let size: Int = arrayOfIntegers.count
-  var total = 0
-  for counter in arrayOfIntegers {
-    total = total + counter
-  }
-  let meanValue: Double = Double(total) / Double(size)
-  return meanValue
+// Mean.
+func mean(arrayOfNumbers: [Int], quantity: Int) throws -> Double {
+    // Variables.
+    var sum = 0
+    // Constants
+    let mean: Double
+
+    for counter in arrayOfNumbers {
+        sum += counter
+    }
+    mean = Double(sum) / Double(quantity)
+
+    return mean
 }
 
-func median(arrayOfIntegers: [Int]) throws -> Double {
-  // this is the median function.
-  var returnValue: Double
-  var array = arrayOfIntegers
-  array.sort()
-  let numberOfNumbers = array.count
-  if numberOfNumbers % 2 == 0 {
-    let numberOfMedian1 = (numberOfNumbers / 2) - 1
-    let median1: Int = array[numberOfMedian1]
-    let numberOfMedian2 = numberOfNumbers / 2
-    let median2: Int = array[numberOfMedian2]
-    returnValue = Double(median1 + median2) / 2
-  } else {
-    let numberOfMedian = Int((Double(numberOfNumbers) / 2) + 0.5)
-    returnValue = Double(array[numberOfMedian])
-  }
-  return returnValue
+// Median.
+func median(arrayOfNumbers: [Int], quantity: Int) throws -> Double {
+    // Variables.
+    var returnValue: Double
+    var array = arrayOfNumbers
+    var median: Double
+    let extra: Double = 0.5
 
+    array.sort()
+
+    if quantity % 2 == 0 {
+        median = Double(array[quantity / 2])
+        returnValue = median
+    } else {
+        median = Double((array[Int(Double(quantity) / 2 - extra)]) +
+           (array[Int(Double(quantity) / 2 + extra)])) / 2
+        returnValue = median
+    }
+
+    return returnValue
 }
 
-func mode(arrayOfIntegers: [Int]) throws -> Double {
-  // this is the mean function.
-  return 1.0
+// Mode.
+func mode(arrayOfNumbers: [Int], quantity: Int) throws -> [Int] {
+    // Variables.
+    var modes = Set<Int>()
+    var array = arrayOfNumbers
+    var maxCount = 0
+
+    modes = [0]
+    array.sort()
+
+    for counterOne in array {
+        var currentCount = 0
+
+        // If the first value of the array equals any of the others
+        // it will add to the count.
+        for counterTwo in array where counterOne == counterTwo {
+            currentCount += 1
+        }
+
+        // If the current count is a new max count,
+        // then it will replace the old one
+        if currentCount > maxCount {
+            maxCount = currentCount
+            modes.removeAll()
+            modes.insert(counterOne)
+        } else if currentCount == maxCount {
+            if modes.contains(counterOne) != true {
+                modes.insert(counterOne)
+            }
+        }
+    }
+    let returnValue = Array(modes)
+    return returnValue
 }
 
-// get arguments
+// Inputs.
 let input = CommandLine.arguments
-let file = input[1] //this is the file. we will write to and read from it
+// This file will be read.
+let file = input[1]
 var array: [String] = []
 var arrayInt: [Int] = []
-// FileManager is the way to write and read files in swift
-if let dir = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+
+// FileManager lets me read files in swift
+// It reads a file from the document Directory in home.
+if let dir = FileManager.default.urls(for: .documentDirectory,
+                                      in: .userDomainMask).first {
+
+    // This adds a trailing "/" if the path component is a directory.
     let fileURL = dir.appendingPathComponent(file)
     do {
-        // how to read the file.
+        // Read file.
         let text = try String(contentsOf: fileURL, encoding: .utf8)
-        array = text.components(separatedBy: .newlines)
-    }
-    catch {print("error")}
-} else { print("error2") }
 
-for counter in array {
-  arrayInt.append(Int(counter)!)
+        // Makes an array from the values in the file,
+        // and then separates each value by a new line.
+        array = text.components(separatedBy: .newlines)
+
+    // If the file cannot be read then it will print this error.
+    } catch {print("File cannot be read properly.")}
+
+// If the file cannot be found then it will print this error.
+} else { print("File cannot be found.") }
+
+for counter in array where !counter.isEmpty {
+    arrayInt.append(Int(counter)!)
 }
 print(arrayInt)
 
-print("\nCalculating stats...")
-let meanPrint = try mean(arrayOfIntegers:arrayInt)
-let medianPrint = try median(arrayOfIntegers:arrayInt)
-let modePrint = try mode(arrayOfIntegers:arrayInt)
+let numberOfValues: Int = array.count
 
-print("The mean is: \(meanPrint)")
-print("The median is: \(medianPrint)")
-print("The mode is: \(modePrint)")
+print("\nCalculating stats...")
+let meanPrint = try mean(arrayOfNumbers: arrayInt, quantity: numberOfValues)
+let medianPrint = try median(arrayOfNumbers: arrayInt, quantity: numberOfValues)
+let modePrint = try mode(arrayOfNumbers: arrayInt, quantity: numberOfValues)
+
+print("The mean is: ", meanPrint)
+print("The median is: ", medianPrint)
+print("The mode is: ", modePrint)
 
 print("\nDone.\n")
